6c531c8328ba74da0f5675515ae037f6c18b4e49
Arbaz Khan <arbazkhan.edu13@gmail.com>
2024-08-27 12:26:10 +0530

    Added styling to index page and installed ejs-mate for better templating

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 //import the schema
 const Listing = require("./models/listing.js");
 
@@ -35,6 +36,8 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
   res.send("working root");
