17ec6365de153c119f4e329088ba77c9dbf1ef19
Arbaz Khan <arbazkhan.edu13@gmail.com>
2024-08-31 15:32:48 +0530

    Fixed error result is not defined

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.get("/", (req, res) => {
 const validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
   if (error) {
-    throw new ExpressError(400, result.error);
+    throw new ExpressError(400, error);
   } else {
     next();
   }
